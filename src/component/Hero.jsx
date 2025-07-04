9378c8713dfac82f21ff638cbdaa2129c46bb343
Nitesh Chaughule <nitesh.chaughule5@gmail.com>
2025-07-04 11:56:26 +0530

    Update and modify Hero section and added new favicon

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
 import { useRef } from "react";
 import { FcViewDetails } from "react-icons/fc";
+import { TypeAnimation } from "react-type-animation";
+
 export default function Hero({ toContact }) {
     return (
         <section className="text-center space-y-4">
@@ -12,21 +14,45 @@ export default function Hero({ toContact }) {
             >
                 Hi, I'm Nitesh
             </motion.h1>
-            <motion.p
-                className="text-lg md:text-xl text-sky-700 dark:text-sky-300 mt-10"
+
+            <motion.div
+                className="text-xl md:text-2xl text-sky-600 dark:text-sky-300 mt-4 font-medium"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
-                transition={{ delay: 0.3, duration: 0.6 }}
+                transition={{ delay: 0.2, duration: 0.6 }}
             >
-                I’m a frontend developer helping businesses build responsive email templates &
+            <TypeAnimation
+                sequence={[
+                "Frontend Developer",
+                1500,
+                "Crafts Email Templates with Tailwind",
+                2000,
+                "React + HTML + CSS Developer",
+                1500,
+                //"Building Modern UIs",
+                ]}
+                wrapper="span"
+                speed={60}
+                repeat={Infinity}
+            />
+            </motion.div>
+
+            <motion.p
+            className="text-lg md:text-xl text-sky-700 dark:text-sky-300 mt-8"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.3, duration: 0.6 }}
+            >
+            I’m a frontend developer helping businesses build responsive email templates &
             </motion.p>
+
             <motion.p
-                className="text-lg md:text-xl text-sky-700 dark:text-sky-300"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.3, duration: 0.6 }}
+            className="text-lg md:text-xl text-sky-700 dark:text-sky-300"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.4, duration: 0.6 }}
             >
-                modern UIs using HTML, Tailwind, and React.
+            modern UIs using HTML, Tailwind, and React.
             </motion.p>
             <div className="flex space-x-4 mt-8 items-center justify-center">
                 <button
