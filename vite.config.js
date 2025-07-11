ec7845aae80fe5340a711be13ed03fea4367ccfb
Nitesh Chaughule - Web Developer <nitesh.chaughule5@gmail.com>
2025-07-11 14:52:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   darkMode: 'class',
-  base: "/nitesh-portfolio-site/",
+  base: "/",
   plugins: [
     [react(), tailwindcss(),]
   ],
