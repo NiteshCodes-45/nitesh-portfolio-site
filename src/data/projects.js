c441545b79ca2e963fadcd22db2b7d29e5186e45
Nitesh Chaughule <nitesh.chaughule5@gmail.com>
2025-07-16 19:09:02 +0530

    slider added for featurework

---
+++ src/data/projects.js
@@ -0,0 +1,36 @@
+const projects = [
+    {
+      title: "FoodCart",
+      description: "Browse and filter Indian dishes, add to cart, and place your order with ease.",
+      image: "./projects/homepage-foodcart.png",
+      link: "#",
+      tags: ['React', 'HTML', 'CSS'],
+      type: "web",
+    },
+    {
+      title: "Live Cricket Scoreboard",
+      description: "Stay updated with live cricket scores, player stats, and match summaries.",
+      image: "./projects/live-homepage.png",
+      link: "https://live-score-cart.vercel.app/",
+      tags: ["React", "Firebase", "Tailwind"],
+      type: "web",
+    },
+    {
+      title: "Responsive Invoice Email Template",
+      description: "Clean and mobile-friendly invoice email designed for clear billing communication across devices.",
+      image: "./projects/full-email-template.png",
+      link: "./portfolio-templates/invoice-email-template.html",
+      tags: ["HTML", "Tailwind", "Email"],
+      type: "email",
+    },
+    {
+      title: "Product Launch Email Template",
+      description: "Promotional email layout crafted to highlight new product features and drive user engagement.",
+      image: "./projects/product-launch-ss.png",
+      link: "./portfolio-templates/product-launch-email-template.html",
+      tags: ["UI", "Tailwind", "Email"],
+      
+    },
+];
+
+export default projects;
